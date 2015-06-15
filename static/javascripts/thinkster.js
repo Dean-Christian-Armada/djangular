8fdfd357bc151020e5d8d7265331f34e5d430460
Dean Christian Armada <deanarmada@gmail.com>
2015-06-15 01:10:54 -0700

    Unfinished djangular app

---
+++ static/javascripts/thinkster.js
@@ -0,0 +1,26 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('thinkster', [
+			'thinkster.routes',
+			'thinkster.authentication',
+			'thinkster.config'
+		]);
+
+	angular.module('thinkster').run(run);
+	run.$inject = ['$http'];
+
+	angular.module('thinkster.routes', ['ngRoute']);
+	angular.module('thinkster.config', []);
+
+	/**
+	* @name run
+	* @desc Update xsrf $http headers to align with Django's defaults
+	*/
+
+	function run($http){
+		$http.defaults.xsrfHeaderName = 'X-CSRFToken';
+		$http.defaults.xsrfCookieName = 'csrftoken';
+	}
+})();
